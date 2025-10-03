5af33efc1f3d98497b7921332892e5503324d1af
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-03 13:31:15 +0100

    #Milestone-1: improvements

--- src/main/java/com/znaji/hibernate/practice/domain/entity/Seller.java
+++ src/main/java/com/znaji/hibernate/practice/domain/entity/Seller.java
@@ -1,11 +1,9 @@
 package com.znaji.hibernate.practice.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor @AllArgsConstructor
@@ -20,8 +18,23 @@ public class Seller {
     @JoinColumn(name = "id")
     private UserAccount userAccount;
 
-    @OneToMany(mappedBy = "seller", orphanRemoval = true)
-    private List<Shop> shops;
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "seller", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Shop> shops = new ArrayList<>();
+
+    public List<Shop> getShops() {
+        return List.copyOf(shops);
+    }
+    public void addShop(Shop shop) {
+        shops.add(shop);
+        shop.setSeller(this);
+    }
+
+    public void removeShop(Shop shop) {
+        shops.remove(shop);
+        shop.setSeller(null);
+    }
 
     @Override
     public boolean equals(Object obj) {
--- src/main/java/com/znaji/hibernate/practice/domain/entity/Shop.java
+++ src/main/java/com/znaji/hibernate/practice/domain/entity/Shop.java
@@ -1,16 +1,12 @@
 package com.znaji.hibernate.practice.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter
+@Getter @Setter()
 @Entity
 public class Shop {
 
@@ -18,11 +14,34 @@ public class Shop {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private Set<String> tags;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ElementCollection
+    @CollectionTable(name = "shop_tags", joinColumns = @JoinColumn(name = "shop_id"))
+    @Column(name = "tag")
+    private Set<String> tags = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "seller_id")
     private Seller seller;
+
+    @Setter(AccessLevel.NONE)
+    @Getter(AccessLevel.NONE)
+    @OneToMany(mappedBy = "shop", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return List.copyOf(products);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        product.setShop(this);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+        product.setShop(null);
+    }
 
     @Override
     public int hashCode() {
